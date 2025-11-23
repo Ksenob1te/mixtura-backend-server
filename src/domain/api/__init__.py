681439e18147f95405fa22cb4237ee2ab65b5059
Dmitriy <dm.elackov@gmail.com>
2025-11-23 17:03:53 +0300

    Init for root router

--- src/domain/api/__init__.py
+++ src/domain/api/__init__.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter
 
 from src.domain.api.member_customs import MemberCustomsController
 from src.domain.api.members import MembersController
-from src.domain.api.server_core import ServerCoreController
-from src.domain.api.server_game_roles import ServerGameRolesController
-from src.domain.api.server_games import ServerGamesController
-from src.domain.api.server_invites import ServerInvitesController
-from src.domain.api.server_ratings import ServerRatingsController
+from src.domain.api.core import ServerCoreController
+from src.domain.api.game_roles import ServerGameRolesController
+from src.domain.api.games import ServerGamesController
+from src.domain.api.invites import ServerInvitesController
+from src.domain.api.ratings import ServerRatingsController
 
 router = APIRouter(
     prefix="/api/servers",
