4b5077b7541a7bfdaeb0809ad9ed392190e57d03
Dmitriy <dm.elackov@gmail.com>
2026-01-07 17:21:06 +0300

    Invite schemas change

--- src/domain/models/invites/response.py
+++ src/domain/models/invites/response.py
@@ -2,16 +2,19 @@ from uuid import UUID
 from pydantic import BaseModel, ConfigDict
 
 from src.domain.models.core.response import ServerListResponse
+from ..member.response import MemberResponse, ReducedMemberResponse
 
 
 class InviteAdminResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-
+    
+    id: UUID
     key: str
-    inviter_id: UUID
+    inviter: MemberResponse
     use_limit: int
 
 class InviteKeyResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
+    inviter: ReducedMemberResponse
     server: ServerListResponse
